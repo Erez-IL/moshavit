2f0790f83d80e9879828a3c23b4acdbc09cc729b
liqweed <ophir.r@gmail.com>
2013-02-18 00:06:47 +0200

    Getting UserRepositoryTest to work

--- src/test/java/com/moshavit/persistence/UserRepositoryTest.java
+++ src/test/java/com/moshavit/persistence/UserRepositoryTest.java
@@ -5,10 +5,8 @@
  */
 package com.moshavit.persistence;
 
-import com.moshavit.framework.persistence.PersistenceConfiguration;
+import com.moshavit.framework.StandardAnnotatedSpringConfiguration;
 import com.moshavit.model.User;
-import org.hibernate.Query;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,19 +16,23 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Inject;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes= PersistenceConfiguration.class)
+@ContextConfiguration(classes= {StandardAnnotatedSpringConfiguration.class})
 public class UserRepositoryTest {
 
 	@Inject
 	private SessionFactory sessionFactory;
+	@Inject UserRepository userRepository;
 
 	@Test
 	@Transactional
-	public void updateAccount() {
-		Session session = sessionFactory.getCurrentSession();
-		Query query = session.createQuery("from User user where user.id=:id").setInteger("id", 1);
-		User user = (User) query.uniqueResult();
+	public void addUser() {
+		User user = new User();
 		user.setFirstName("moshe");
+		Long userId = userRepository.addUser(user);
+		User fetchedUser = (User) sessionFactory.getCurrentSession().get(User.class, userId);
+		assertEquals("moshe", fetchedUser.getFirstName());
 	}
 }
