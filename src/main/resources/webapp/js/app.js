ede60839bd0e6d4e278b6c9c68ca9a39d8151dc1
Erez <erez83@gmail.com>
2013-02-17 22:56:15 +0200

    JavaScripts : addUser,UpdateUser, usersTableClickUpdateForm

--- src/main/resources/webapp/js/app.js
+++ src/main/resources/webapp/js/app.js
@@ -1,3 +1,4 @@
+//Create a template page by template name
 var getTemplate = function (templateName, callback) {
 	$.get("/templates/" + templateName + ".hbs", function (data) {
 		var template = Handlebars.compile(data);
@@ -5,24 +6,50 @@ var getTemplate = function (templateName, callback) {
 		callback(template);
 	});
 };
+
+//get current date to string dd/mm/YY
+var getDateNow = function () {
+	var currentTime = new Date();
+	var month = currentTime.getMonth() + 1;
+	var day = currentTime.getDate();
+	var year = currentTime.getFullYear();
+	return "" + day + "/" + month + "/" + year + "";
+};
+
+//update Option to Users Table
+var updateOptionToUsersTable = function () {
+	$('#UserTableList tbody tr').click(function () {
+		var idNum = $(this).find("td.ID").text();
+		getTemplate("updateUserForm", function (template) {
+			var jsonID = "/api/users/" + idNum;
+			$.getJSON(jsonID, function (users) {
+				console.log("Got users: ", users);
+				var usersTable = template(users);
+				$('div.registrationForm').append(usersTable);
+			});
+			//remove the UserTableList
+			$('#UserTableList').remove();
+		});
+	});
+};
+
 getTemplate("users", function (template) {
 	$.getJSON("/api/users", function (users) {
 		console.log("Got users: ", users);
 		var usersTable = template(users);
 		$('div.users').append(usersTable);
-
+		//update Option to Table
+		updateOptionToUsersTable();
 	});
 });
 
 $("#addUser").click(function () {
-			getTemplate("registerUserForm", function (template) {
-				$('div.registrationForm').append(template);
+	getTemplate("registerUserForm", function (template) {
+		$('div.registrationForm').append(template);
+		//remove the UserTableList
+		$('#UserTableList').remove();
+	});
+});
 
-				//remove the UserTableList
-								var div = document.getElementById("UserTableList");
-								div.parentNode.removeChild(div);
-			})
-		}
-);
 
 
--- src/main/resources/webapp/js/userService.js
+++ src/main/resources/webapp/js/userService.js
@@ -5,39 +5,68 @@
  * Time: 11:02 PM
  * To change this template use File | Settings | File Templates.
  */
-$(function () { $("input,select,textarea").not("[type=submit]").jqBootstrapValidation(); } );
+$(function () { $("input,select,textarea").not("[type=submit]").jqBootstrapValidation(); });
 $("#addUserFromForm").click(function () {
-		console.log("Creating user...");
-			var currentTime = new Date();
-			var month = currentTime.getMonth() + 1;
-			var day = currentTime.getDate();
-			var year = currentTime.getFullYear();
-		$.ajax({
-			url: "/api/users",
-			type: "POST",
-			contentType: "application/json",
-			data: JSON.stringify({
-				username:  document.getElementById('username').value,
-				firstName:  document.getElementById('firstName').value,
-				lastName: document.getElementById('lastName').value,
-				dateOfIssue: ""+ day + "/" + month + "/" + year+"",
-				membership: document.getElementById('membership').value
+	console.log("Creating user...");
+	$.ajax({
+		url: "/api/users",
+		type: "POST",
+		contentType: "application/json",
+		data: JSON.stringify({
+			username: document.getElementById('username').value,
+			firstName: document.getElementById('firstName').value,
+			lastName: document.getElementById('lastName').value,
+			email: document.getElementById('email').value,
+			dateOfIssue: getDateNow(),
+			membership: document.getElementById('membership').value,
+			dateOfLastUpdate: getDateNow()
+		}), success: function (data) {
+			console.log("Added " + data);
+			//restore UserTableList and close this form
+			restoreUsersTable();
+		}});
+});
 
-			}), success: function(data) {
-				console.log("Added " + data);
-
-				//remove the Register Form
-				var div = document.getElementById("RegisterForm");
-				div.parentNode.removeChild(div);
-
-				//retrieve UserTableList
-				getTemplate("users", function (template) {
-					$.getJSON("/api/users", function (users) {
-						console.log("Got users: ", users);
-						var usersTable = template(users);
-						$('div.users').append(usersTable);
-					});
-				});
-			}});
+//restore UserTableList
+var restoreUsersTable = function () {
+	//remove the Register Form
+	var div = document.getElementById("RegisterForm");
+	if (div !== null)div.parentNode.removeChild(div);
+	//remove the Update Form
+	var div = document.getElementById("updateForm");
+	if (div !== null)div.parentNode.removeChild(div);
+	getTemplate("users", function (template) {
+		$.getJSON("/api/users", function (users) {
+			console.log("Got users: ", users);
+			var usersTable = template(users);
+			$('div.users').append(usersTable);
+			updateOptionToUsersTable();
+		});
 	});
+};
+$("#cancelForm").click(function () {
+	restoreUsersTable();
+});
+
+$("#updateUserFromForm").click(function () {
+	console.log("Update user...");
+	$.ajax({
+		url: "/api/users/update",
+		type: "POST",
+		contentType: "application/json",
+		data: JSON.stringify({
+			id: document.getElementById('userID').value,
+			username: document.getElementById('username').value,
+			firstName: document.getElementById('firstName').value,
+			lastName: document.getElementById('lastName').value,
+			dateOfIssue: document.getElementById('dateOfIssue').value,
+			email: document.getElementById('email').value,
+			membership: document.getElementById('membership').value,
+			dateOfLastUpdate: getDateNow()
+		}), success: function (data) {
+			console.log("Update  " + data);
+			//restore UserTableList and close this form
+			restoreUsersTable();
+		}});
+});
 
