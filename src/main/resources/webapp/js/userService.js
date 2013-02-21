68003aecdb4211ee01c0dc7e3e430561d642f4f6
liqweed <ophir.r@gmail.com>
2013-02-21 22:35:36 +0200

    wrapped userService.js in bindUserFormElements function and invoking it

--- src/main/resources/webapp/js/userService.js
+++ src/main/resources/webapp/js/userService.js
@@ -1,38 +1,35 @@
-/**
- * Created with IntelliJ IDEA.
- * User: erez
- * Date: 2/15/13
- * Time: 11:02 PM
- * To change this template use File | Settings | File Templates.
- */
-$(function () { $("input,select,textarea").not("[type=submit]").jqBootstrapValidation(); });
-$("#addUserFromForm").click(function () {
-	console.log("Creating user...");
-	$.ajax({
-		url: "/api/users",
-		type: "POST",
-		contentType: "application/json",
-		data: createJsonFromForm(), success: function (data) {
-			console.log("Added " + data);
-			//restore UserTableList and close this form
-			restoreUsersTable();
-		}});
-});
+bindUserFormElements = function() {
+	$("#addUserFromForm").click(function () {
+		console.log("Creating user...");
+		$.ajax({
+			url: "/api/users",
+			type: "POST",
+			contentType: "application/json",
+			data: createJsonFromForm(), success: function (data) {
+				console.log("Added " + data);
+				//restore UserTableList and close this form
+				restoreUsersTable();
+			}});
+	});
 
-$("#cancelForm").click(function () {
-	restoreUsersTable();
-});
+	$("#cancelForm").click(function () {
+		restoreUsersTable();
+	});
 
-$("#updateUserFromForm").click(function () {
-	console.log("Update user...");
-	$.ajax({
-		url: "/api/users/"+$('#userID').val(),
-		type: "PUT",
-		contentType: "application/json",
-		data: createJsonFromForm(), success: function (data) {
-			console.log("Update  " + data);
-			//restore UserTableList and close this form
-			restoreUsersTable();
-		}});
-});
+	$("#updateUserFromForm").click(function () {
+		console.log("Update user...");
+		$.ajax({
+			url: "/api/users/"+$('#userID').val(),
+			type: "PUT",
+			contentType: "application/json",
+			data: createJsonFromForm(), success: function (data) {
+				console.log("Update  " + data);
+				//restore UserTableList and close this form
+				restoreUsersTable();
+			}});
+	});
 
+	$(function () { $("input,select,textarea").not("[type=submit]").jqBootstrapValidation(); });
+};
+
+bindUserFormElements();
