1a40bbab2bf22ee7623b50f6a386cc0afa3af54f
Erez <erez83@gmail.com>
2013-02-24 01:08:12 +0200

    add MessageFormService and Form

--- src/main/resources/webapp/js/app.js
+++ src/main/resources/webapp/js/app.js
@@ -13,14 +13,73 @@ var getTemplateHBS = function (templateName, callback) {
 		callback(template);
 	});
 };
-//JSON createJsonFromForm
-var stringifyJSON = function () {
+//JSON from User Detail's
+var stringifyUserJSON = function () {
 	return JSON.stringify({
 		username: $('#username').val(),
 		firstName: $('#firstName').val(),
 		lastName: $('#lastName').val(),
 		email: $('#email').val(),
 		membership: $('#membership').val()
+	})
+};
+//JSON from Message Detail's
+var stringifyMessageJSON = function () {
+	return JSON.stringify({
+		subject: $('#subject').val(),
+		messageText: $('#messageText').val(),
+		author: {
+		        "id": 1,
+		        "firstName": "",
+		        "lastName": "",
+		        "membership": "resident",
+		        "username": "erez",
+		        "email": "",
+		        "dateOfIssue": {
+		            "era": 1,
+		            "dayOfMonth": 24,
+		            "dayOfWeek": 7,
+		            "dayOfYear": 55,
+		            "weekyear": 2013,
+		            "monthOfYear": 2,
+		            "weekOfWeekyear": 8,
+		            "centuryOfEra": 20,
+		            "millisOfSecond": 647,
+		            "millisOfDay": 3041647,
+		            "yearOfEra": 2013,
+		            "yearOfCentury": 13,
+		            "secondOfMinute": 41,
+		            "secondOfDay": 3041,
+		            "minuteOfHour": 50,
+		            "minuteOfDay": 50,
+		            "hourOfDay": 0,
+		            "year": 2013,
+		            "zone": {
+		                "uncachedZone": {
+		                    "cachable": true,
+		                    "fixed": false,
+		                    "id": "Asia/Jerusalem"
+		                },
+		                "fixed": false,
+		                "id": "Asia/Jerusalem"
+		            },
+		            "millis": 1361659841647,
+		            "chronology": {
+		                "zone": {
+		                    "uncachedZone": {
+		                        "cachable": true,
+		                        "fixed": false,
+		                        "id": "Asia/Jerusalem"
+		                    },
+		                    "fixed": false,
+		                    "id": "Asia/Jerusalem"
+		                }
+		            },
+		            "afterNow": false,
+		            "beforeNow": true,
+		            "equalNow": false
+		        }
+		    }
 	})
 };
 
@@ -46,7 +105,9 @@ var restoreUsersTable = function () {
 	//empty the userFormContainer
 	$('div.userFormContainer').empty();
 	renderUsers();
+
 };
+
 //restore UserTableList  new Style
 var buildUsersTable = function () {
 	//remove the userFormModel
@@ -55,6 +116,20 @@ var buildUsersTable = function () {
 	renderUsersTable();
 };
 
+//restore Messages Form
+var buildMessageForm = function () {
+	$('div.addMessageForm').empty();
+	renderMessageForm();
+};
+var renderMessageForm = function () {
+	getTemplateHBS("messageForm", function (template) {
+		$.getJSON("/api/boardMessage", function (messages) {
+			console.log("Got Messages: ", messages);
+			var messageForm = template(messages);
+			$('div.addMessageForm').append(messageForm);
+		});
+	});
+};
 var renderUsers = function () {
 	getTemplateHBS("users", function (template) {
 		$.getJSON("/api/users", function (users) {
@@ -67,31 +142,39 @@ var renderUsers = function () {
 	});
 };
 var renderUsersTable = function () {
-renderUsers();
+	renderUsers();
 };
 
-
-$(document).ready(function() {
+$(document).ready(function () {
 	renderUsersTable();
 	bindUserFormElements();
+	renderMessageForm();
 	$("#addUser").click(function () {
 		getTemplateHBS("registerUserForm", function (template) {
-			var registrationForm = template({});
-			$('div.userFormContainer').append(registrationForm);
+			$('div.userFormContainer').append(template);
 			bindUserFormElements();
 			//remove the UserTableList
 			$('#UserTableList').remove();
 		});
 	});
 	//new Style of reg form
-		$("#addNew").click(function () {
-			getTemplateHBS("newRegisterForm", function (template) {
-				$('body').append(template);
-				bindUserFormElements();
-				//remove the UserTableList
-				$('#UserTableList').remove();
-				$('#myModal').modal('show');
-			});
+	$("#addNew").click(function () {
+		getTemplateHBS("newRegisterForm", function (template) {
+			$('body').append(template);
+			bindUserFormElements();
+			//remove the UserTableList
+			$('#UserTableList').remove();
+			$('#myModal').modal('show');
 		});
-
+	});
+	//add message form
+	$("#addMessage").click(function () {
+		getTemplateHBS("addMessageForm", function (template) {
+			$('body').append(template);
+			bindUserFormElements();
+			//remove the UserTableList
+			$('#UserTableList').remove();
+			bindMessageFormElements();
+		});
+	});
 });
