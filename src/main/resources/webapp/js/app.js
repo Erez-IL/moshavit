2ca1cf993d9282028afc11daf71d75eddbda07b1
Erez <erez83@gmail.com>
2013-02-23 13:47:39 +0200

    clear duplicate stuff

--- src/main/resources/webapp/js/app.js
+++ src/main/resources/webapp/js/app.js
@@ -1,14 +1,20 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: erez
+ * Date: 2/22/13
+ * Time: 9:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
 //Create a template page by template name
-var getTemplate = function (templateName, callback) {
+var getTemplateHBS = function (templateName, callback) {
 	$.get("/templates/" + templateName + ".hbs", function (data) {
 		var template = Handlebars.compile(data);
 		console.log("Compiled " + templateName + " template.");
 		callback(template);
 	});
 };
-
 //JSON createJsonFromForm
-var createJsonFromForm = function () {
+var stringifyJSON = function () {
 	return JSON.stringify({
 		username: $('#username').val(),
 		firstName: $('#firstName').val(),
@@ -19,10 +25,10 @@ var createJsonFromForm = function () {
 };
 
 //update Option to Users Table
-var updateOptionToUsersTable = function () {
+var updateOptionOnUsersTable = function () {
 	$('#UserTableList tbody tr').click(function () {
 		var idNum = $(this).find("td.ID").text();
-		getTemplate("updateUserForm", function (template) {
+		getTemplateHBS("updateUserForm", function (template) {
 			var jsonID = "/api/users/" + idNum;
 			$.getJSON(jsonID, function (users) {
 				console.log("Got users: ", users);
@@ -35,41 +41,41 @@ var updateOptionToUsersTable = function () {
 		});
 	});
 };
-
-//restore UserTableList
+//restore UserTableList old style
 var restoreUsersTable = function () {
 	//empty the userFormContainer
 	$('div.userFormContainer').empty();
 	renderUsers();
 };
+//restore UserTableList  new Style
+var buildUsersTable = function () {
+	//remove the userFormModel
+	$('#myModal').modal('hide')
+	$('#myModal').remove();
+	renderUsersTable();
+};
 
 var renderUsers = function () {
-	getTemplate("users", function (template) {
+	getTemplateHBS("users", function (template) {
 		$.getJSON("/api/users", function (users) {
 			console.log("Got users: ", users);
 			var usersTable = template(users);
 			$('div.users').append(usersTable);
 			//update Option to Table
-			updateOptionToUsersTable();
+			updateOptionOnUsersTable();
 		});
 	});
 };
-
-//getTemplate("users", function (template) {
-//	$.getJSON("/api/users", function (users) {
-//		console.log("Got users: ", users);
-//		var usersTable = template(users);
-//		$('div.users').append(usersTable);
-//		//update Option to Table
-//		updateOptionToUsersTable();
-//	});
-//});
+var renderUsersTable = function () {
+renderUsers();
+};
 
 
 $(document).ready(function() {
+	renderUsersTable();
 	bindUserFormElements();
 	$("#addUser").click(function () {
-		getTemplate("registerUserForm", function (template) {
+		getTemplateHBS("registerUserForm", function (template) {
 			var registrationForm = template({});
 			$('div.userFormContainer').append(registrationForm);
 			bindUserFormElements();
@@ -77,4 +83,15 @@ $(document).ready(function() {
 			$('#UserTableList').remove();
 		});
 	});
+	//new Style of reg form
+		$("#addNew").click(function () {
+			getTemplateHBS("newRegisterForm", function (template) {
+				$('body').append(template);
+				bindUserFormElements();
+				//remove the UserTableList
+				$('#UserTableList').remove();
+				$('#myModal').modal('show');
+			});
+		});
+
 });
--- src/main/resources/webapp/js/userService.js
+++ src/main/resources/webapp/js/userService.js
@@ -5,70 +5,67 @@
  * Time: 11:02 PM
  * To change this template use File | Settings | File Templates.
  */
-var bindUserFormElements = function() {
-$("#addUserFromForm").click(function () {
-	console.log("Creating user...");
-	$.ajax({
-		url: "/api/users",
-		type: "POST",
-		contentType: "application/json",
-		data: createJsonFromForm(), success: function (data) {
-			console.log("Added " + data);
-			//restore UserTableList and close this form
-			restoreUsersTable();
-		}});
-});
+var bindUserFormElements = function () {
+	$("#addUserFromForm").click(function () {
+		console.log("Creating user...");
+		$.ajax({
+			url: "/api/users",
+			type: "POST",
+			contentType: "application/json",
+			data: stringifyJSON(), success: function (data) {
+				console.log("Added " + data);
+				//restore UserTableList and close this form
+				restoreUsersTable();
+			}});
+	});
 
-$("#cancelForm").click(function () {
-	restoreUsersTable();
-});
+	$("#cancelForm").click(function () {
+		restoreUsersTable();
+	});
 
-$("#updateUserFromForm").click(function () {
-	console.log("Update user...");
-	$.ajax({
-		url: "/api/users/"+$('#userID').val(),
-		type: "PUT",
-		contentType: "application/json",
-		data: createJsonFromForm(), success: function (data) {
-			console.log("Update  " + data);
-			//restore UserTableList and close this form
-			restoreUsersTable();
-		}});
-});
-
-
-
+	$("#updateUserFromForm").click(function () {
+		console.log("Update user...");
+		$.ajax({
+			url: "/api/users/" + $('#userID').val(),
+			type: "PUT",
+			contentType: "application/json",
+			data: stringifyJSON(), success: function (data) {
+				console.log("Update  " + data);
+				//restore UserTableList and close this form
+				restoreUsersTable();
+			}});
+	});
 
 //new style JS
-$('.closeForm').click(function () {
-	buildUsersTable();
-});
+	$('.closeForm').click(function () {
+		buildUsersTable();
+	});
 
-$("#submitForm").click(function () {
-	console.log("Creating user...");
-	$.ajax({
-		url: "/api/users",
-		type: "POST",
-		contentType: "application/json",
-		data: stringifyJSON(), success: function (data) {
-			console.log("Added " + data);
-			//restore UserTableList and close this form
-			buildUsersTable();
-		}});
-});
+	$("#submitForm").click(function () {
+		console.log("Creating user...");
+		$.ajax({
+			url: "/api/users",
+			type: "POST",
+			contentType: "application/json",
+			data: stringifyJSON(), success: function (data) {
+				console.log("Added " + data);
+				//restore UserTableList and close this form
+				buildUsersTable();
+			}});
+	});
 
-$("#updateSubmitForm").click(function () {
-	console.log("Update user...");
-	$.ajax({
-		url: "/api/users/"+$('#userID').val(),
-		type: "PUT",
-		contentType: "application/json",
-		data: stringifyJSON(), success: function (data) {
-			console.log("Update  " + data);
-			//restore UserTableList and close this form
-			buildUsersTable();
-		}});
-});
+	$("#updateSubmitForm").click(function () {
+		console.log("Update user...");
+		$.ajax({
+			url: "/api/users/" + $('#userID').val(),
+			type: "PUT",
+			contentType: "application/json",
+			data: stringifyJSON(), success: function (data) {
+				console.log("Update  " + data);
+				//restore UserTableList and close this form
+				buildUsersTable();
+			}});
+	});
 	$(function () { $("input,select,textarea").not("[type=submit]").jqBootstrapValidation(); });
 };
 
